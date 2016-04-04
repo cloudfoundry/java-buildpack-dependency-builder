f17e42692af9f047fdf1e75fcdb7fbe0e89068fc
Ben Hale <bhale@pivotal.io>
2016-04-04 15:52:08 -0700

    Replication
    This change updates the replication script for the new structure of the
    dependency builder.

---
+++ replicate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ -z "$BASE_URI" ]]; then
+  echo "BASE_URI must be set" >&2
+  exit 1
+fi
+
+if [[ -z "$DESTINATION" ]]; then
+  echo "DESTINATION must be set" >&2
+  exit 1
+fi
+
+aws s3 sync "s3://download.pivotal.io" "$DESTINATION" --exclude "*" \
+  --include "groovy/*" \
+  --include "tc-server/*" \
+  --include "tomcat/*" \
+  --include "tomcat-access-logging-support/*" \
+  --include "tomcat-lifecycle-support/*" \
+  --include "tomcat-logging-support/*" \
+  --include "tomee/*" \
+  --include "tomee-resource-configuration/*" \
+  --exclude "*/lucid/*"
+
+find $DESTINATION -name "index.yml" | xargs sed -ie "s|https://download.run.pivotal.io|$BASE_URI|g"
