8cf30f700124ad95cb32a6b625602d78afdaedb9
Ben Hale <bhale@pivotal.io>
2016-04-04 14:52:16 -0700

    Tomcat Lifecycle Support Dependency Builder
    This change adds the Tomcat Lifecycle Support Dependency Builder

---
+++ tomcat-lifecycle-support.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $(dirname "$0")/common.sh
+
+download_uri() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  echo "http://repo.spring.io/release/org/cloudfoundry/tomcat-lifecycle-support/$DOWNLOAD_VERSION/tomcat-lifecycle-support-$DOWNLOAD_VERSION.jar"
+}
+
+upload_path() {
+  if [[ -z "$UPLOAD_VERSION" ]]; then
+    echo "UPLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  echo "/tomcat-lifecycle-support/tomcat-lifecycle-support-$UPLOAD_VERSION.jar"
+}
+
+DOWNLOAD_URI=$(download_uri)
+UPLOAD_PATH=$(upload_path)
+INDEX_PATH="/tomcat-lifecycle-support/index.yml"
+
+transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
+update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
