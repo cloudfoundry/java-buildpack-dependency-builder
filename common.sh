68082075cf018d86a379c2f9791e57504202625a
Ben Hale <bhale@pivotal.io>
2016-04-04 13:27:54 -0700

    Tomcat Dependency Builder
    This change adds the Tomcat Dependency Builder

---
+++ common.sh
@@ -0,0 +1,54 @@
+# $@: S3 invalidation paths without bucket
+invalidate_cache() {
+  if [[ -z "$CLOUDFRONT_DISTRIBUTION_ID" ]]; then
+    echo "CLOUDFRONT_DISTRIBUTION_ID must be set" >&2
+    exit 1
+  fi
+
+  aws configure set preview.cloudfront true
+  INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "$@" | jq -r '.Invalidation.Id')
+
+  printf "Waiting for invalidation $INVALIDATION_ID"
+
+  while [[ $(aws cloudfront get-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --id $INVALIDATION_ID | jq -r '.Invalidation.Status') == "InProgress" ]]; do
+    printf "."
+    sleep 10
+  done
+
+  echo
+}
+
+# $1: Download URI
+# $2: S3 path without bucket
+transfer_direct() {
+  if [[ -z "$S3_BUCKET" ]]; then
+    echo "S3_BUCKET must be set" >&2
+    exit 1
+  fi
+
+  SOURCE=$1
+  TARGET="s3://$S3_BUCKET$2"
+
+  echo "$SOURCE -> $TARGET"
+
+  curl --location $SOURCE | aws s3 cp - $TARGET
+}
+
+# $1: S3 index path without bucket
+# $2: Version
+# $3: S3 artifact path without bucket
+update_index() {
+  if [[ -z "$S3_BUCKET" ]]; then
+    echo "S3_BUCKET must be set" >&2
+    exit 1
+  fi
+
+  INDEX_PATH="s3://$S3_BUCKET$1"
+  VERSION=$2
+  DOWNLOAD_URI="https://download.run.pivotal.io$3"
+
+  echo "$VERSION: $DOWNLOAD_URI -> $INDEX_PATH"
+
+  (aws s3 cp $INDEX_PATH - 2> /dev/null || echo '---') | printf -- "$(cat -)\n$VERSION: $DOWNLOAD_URI\n" | aws s3 cp - $INDEX_PATH
+
+}
---
+++ tomcat.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $(dirname "$0")/common.sh
+
+# $1: The Tomcat version
+download_uri() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  if [[ "$DOWNLOAD_VERSION" =~ ^6 ]]; then
+    echo "http://archive.apache.org/dist/tomcat/tomcat-6/v$DOWNLOAD_VERSION/bin/apache-tomcat-$DOWNLOAD_VERSION.tar.gz"
+  elif [[ "$DOWNLOAD_VERSION" =~ ^7 ]]; then
+    echo "http://archive.apache.org/dist/tomcat/tomcat-7/v$DOWNLOAD_VERSION/bin/apache-tomcat-$DOWNLOAD_VERSION.tar.gz"
+  elif [[ "$DOWNLOAD_VERSION" =~ ^8 ]]; then
+    echo "http://archive.apache.org/dist/tomcat/tomcat-8/v$DOWNLOAD_VERSION/bin/apache-tomcat-$DOWNLOAD_VERSION.tar.gz"
+  else
+    echo "Unable to process version '$DOWNLOAD_VERSION'" >&2
+    exit 1
+  fi
+}
+
+# $1: The Tomcat version
+upload_path() {
+  if [[ -z "$UPLOAD_VERSION" ]]; then
+    echo "UPLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  echo "/tomcat/tomcat-$UPLOAD_VERSION.tar.gz"
+}
+
+DOWNLOAD_URI=$(download_uri)
+UPLOAD_PATH=$(upload_path)
+INDEX_PATH="/tomcat/index.yml"
+
+transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
