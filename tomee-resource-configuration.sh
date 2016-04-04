6b7293baa709ad2739cf2dbef88643ae6e1c58e9
Ben Hale <bhale@pivotal.io>
2016-04-04 14:37:50 -0700

    TomEE Resource Configuration Dependency Builder
    This change adds the TomEE Resource Configuration Dependency Builder

---
+++ tomee-resource-configuration.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $(dirname "$0")/common.sh
+
+download_uri() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  echo "http://repo.spring.io/release/org/cloudfoundry/tomee-resource-configuration/$DOWNLOAD_VERSION/tomee-resource-configuration-$DOWNLOAD_VERSION.jar"
+}
+
+upload_path() {
+  if [[ -z "$UPLOAD_VERSION" ]]; then
+    echo "UPLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  echo "/tomee-resource-configuration/tomee-resource-configuration-$UPLOAD_VERSION.jar"
+}
+
+DOWNLOAD_URI=$(download_uri)
+UPLOAD_PATH=$(upload_path)
+INDEX_PATH="/tomee-resource-configuration/index.yml"
+
+transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
+update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
