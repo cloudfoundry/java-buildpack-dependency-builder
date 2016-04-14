c0871534093a72e1bd6432cd03eb35ca74e8dc9b
Ben Hale <bhale@pivotal.io>
2016-04-14 09:37:37 -0700

    Java Buildpack Memory Calculator
    This change updates the build for the newest release of the Java Buildpack
    Memory Calculator.

--- memory-calculator.sh
+++ memory-calculator.sh
@@ -4,22 +4,17 @@ set -e
 
 source $(dirname "$0")/common.sh
 
-download_uri_mountainlion() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
+clone_repository() {
+  if [[ -z "$TAG" ]]; then
+    echo "TAG must be set" >&2
     exit 1
   fi
 
-  echo "https://github.com/cloudfoundry/java-buildpack-memory-calculator/releases/download/v$DOWNLOAD_VERSION/java-buildpack-memory-calculator-darwin.tar.gz"
-}
+  git clone https://github.com/cloudfoundry/java-buildpack-memory-calculator.git gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator
 
-download_uri_trusty() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "https://github.com/cloudfoundry/java-buildpack-memory-calculator/releases/download/v$DOWNLOAD_VERSION/java-buildpack-memory-calculator-linux.tar.gz"
+  pushd gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator
+    git checkout $TAG
+  popd
 }
 
 upload_path_mountainlion() {
@@ -40,15 +35,15 @@ upload_path_trusty() {
   echo "/memory-calculator/trusty/x86_64/memory-calculator-$UPLOAD_VERSION.tar.gz"
 }
 
-DOWNLOAD_URI_MOUNTAINLION=$(download_uri_mountainlion)
-DOWNLOAD_URI_TRUSTY=$(download_uri_trusty)
 UPLOAD_PATH_MOUNTAINLION=$(upload_path_mountainlion)
 UPLOAD_PATH_TRUSTY=$(upload_path_trusty)
 INDEX_PATH_MOUNTAINLION="/memory-calculator/mountainlion/x86_64/index.yml"
 INDEX_PATH_TRUSTY="/memory-calculator/trusty/x86_64/index.yml"
 
-transfer_direct $DOWNLOAD_URI_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION
-transfer_direct $DOWNLOAD_URI_TRUSTY $UPLOAD_PATH_TRUSTY
+clone_repository
+gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator/ci/build.sh
+transfer_to_s3 'gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator/java-buildpack-memory-calculator-darwin.tar.gz' $UPLOAD_PATH_MOUNTAINLION
+transfer_to_s3 'gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator/java-buildpack-memory-calculator-linux.tar.gz' $UPLOAD_PATH_TRUSTY
 update_index $INDEX_PATH_MOUNTAINLION $UPLOAD_VERSION $UPLOAD_PATH_MOUNTAINLION
 update_index $INDEX_PATH_TRUSTY $UPLOAD_VERSION $UPLOAD_PATH_TRUSTY
 invalidate_cache $INDEX_PATH_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION $INDEX_PATH_TRUSTY $UPLOAD_PATH_TRUSTY
