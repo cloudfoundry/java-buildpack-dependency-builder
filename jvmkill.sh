914e7b53d3df77bea58b0c45675c39365ae629cf
Ben Hale <bhale@pivotal.io>
2017-01-25 15:30:04 -0800

    Updated jvmkill builder

--- jvmkill.sh
+++ jvmkill.sh
@@ -4,40 +4,51 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-clone_repository() {
-  if [[ -z "$TAG" ]]; then
-    echo "TAG must be set" >&2
+download_uri_darwin() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
     exit 1
   fi
 
-  git clone https://github.com/cloudfoundry/jvmkill.git
-
-  pushd jvmkill
-    git checkout $TAG
-  popd
+  echo $(spring_release_uri 'org.cloudfoundry' 'jvmkill' $DOWNLOAD_VERSION "-darwin.so")
 }
 
-upload_path() {
-  if [[ -z "$PLATFORM" ]]; then
-    echo "PLATFORM must be set" >&2
+download_uri_trusty() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
     exit 1
   fi
 
+  echo $(spring_release_uri 'org.cloudfoundry' 'jvmkill' $DOWNLOAD_VERSION "-trusty.so")
+}
+
+upload_path_mountainlion() {
   if [[ -z "$UPLOAD_VERSION" ]]; then
     echo "UPLOAD_VERSION must be set" >&2
     exit 1
   fi
 
-  echo "/jvmkill/$PLATFORM/x86_64/jvmkill-$UPLOAD_VERSION.so"
+  echo "/jvmkill/mountainlion/x86_64/jvmkill-$UPLOAD_VERSION.so"
 }
 
-PATH=/usr/local/bin:$PATH
+upload_path_trusty() {
+  if [[ -z "$UPLOAD_VERSION" ]]; then
+    echo "UPLOAD_VERSION must be set" >&2
+    exit 1
+  fi
 
-UPLOAD_PATH=$(upload_path)
-INDEX_PATH="/jvmkill/$PLATFORM/x86_64/index.yml"
+  echo "/jvmkill/trusty/x86_64/jvmkill-$UPLOAD_VERSION.so"
+}
 
-clone_repository
-jvmkill/ci/build.sh
-transfer_to_s3 'jvmkill/libjvmkill.so' $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+DOWNLOAD_URI_DARWIN=$(download_uri_darwin)
+DOWNLOAD_URI_TRUSTY=$(download_uri_trusty)
+UPLOAD_PATH_MOUNTAINLION=$(upload_path_mountainlion)
+UPLOAD_PATH_TRUSTY=$(upload_path_trusty)
+INDEX_PATH_MOUNTAINLION="/jvmkill/mountainlion/x86_64/index.yml"
+INDEX_PATH_TRUSTY="/jvmkill/trusty/x86_64/index.yml"
+
+transfer_direct $DOWNLOAD_URI_DARWIN $UPLOAD_PATH_MOUNTAINLION
+transfer_direct $DOWNLOAD_URI_TRUSTY $UPLOAD_PATH_TRUSTY
+update_index $INDEX_PATH_MOUNTAINLION $UPLOAD_VERSION $UPLOAD_PATH_MOUNTAINLION
+update_index $INDEX_PATH_TRUSTY $UPLOAD_VERSION $UPLOAD_PATH_TRUSTY
+invalidate_cache $INDEX_PATH_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION $INDEX_PATH_TRUSTY $UPLOAD_PATH_TRUSTY
