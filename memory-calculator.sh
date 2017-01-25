f146047d2f7f0ecb035e237d0e7cc6b3700c035b
Ben Hale <bhale@pivotal.io>
2017-01-25 14:53:13 -0800

    New download location for memory-calculator

--- memory-calculator.sh
+++ memory-calculator.sh
@@ -4,17 +4,22 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-clone_repository() {
-  if [[ -z "$TAG" ]]; then
-    echo "TAG must be set" >&2
+download_uri_darwin() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
     exit 1
   fi
 
-  git clone https://github.com/cloudfoundry/java-buildpack-memory-calculator.git gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator
+  echo $(spring_release_uri 'org.cloudfoundry' 'java-buildpack-memory-calculator' $DOWNLOAD_VERSION, "-darwin.tar.gz")
+}
 
-  pushd gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator
-    git checkout $TAG
-  popd
+download_uri_linux() {
+  if [[ -z "$DOWNLOAD_VERSION" ]]; then
+    echo "DOWNLOAD_VERSION must be set" >&2
+    exit 1
+  fi
+
+  echo $(spring_release_uri 'org.cloudfoundry' 'java-buildpack-memory-calculator' $DOWNLOAD_VERSION, "-linux.tar.gz")
 }
 
 upload_path_mountainlion() {
@@ -35,15 +40,15 @@ upload_path_trusty() {
   echo "/memory-calculator/trusty/x86_64/memory-calculator-$UPLOAD_VERSION.tar.gz"
 }
 
+DOWNLOAD_URI_DARWIN=$(download_uri_darwin)
+DOWNLOAD_URI_LINUX=$(download_uri_linux)
 UPLOAD_PATH_MOUNTAINLION=$(upload_path_mountainlion)
 UPLOAD_PATH_TRUSTY=$(upload_path_trusty)
 INDEX_PATH_MOUNTAINLION="/memory-calculator/mountainlion/x86_64/index.yml"
 INDEX_PATH_TRUSTY="/memory-calculator/trusty/x86_64/index.yml"
 
-clone_repository
-gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator/ci/build.sh
-transfer_to_s3 'gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator/java-buildpack-memory-calculator-darwin.tar.gz' $UPLOAD_PATH_MOUNTAINLION
-transfer_to_s3 'gopath/src/github.com/cloudfoundry/java-buildpack-memory-calculator/java-buildpack-memory-calculator-linux.tar.gz' $UPLOAD_PATH_TRUSTY
+transfer_direct $DOWNLOAD_URI_DARWIN $UPLOAD_PATH_MOUNTAINLION
+transfer_direct $DOWNLOAD_URI_LINUX $UPLOAD_PATH_TRUSTY
 update_index $INDEX_PATH_MOUNTAINLION $UPLOAD_VERSION $UPLOAD_PATH_MOUNTAINLION
 update_index $INDEX_PATH_TRUSTY $UPLOAD_VERSION $UPLOAD_PATH_TRUSTY
 invalidate_cache $INDEX_PATH_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION $INDEX_PATH_TRUSTY $UPLOAD_PATH_TRUSTY
