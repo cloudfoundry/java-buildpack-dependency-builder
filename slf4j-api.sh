b03ceaeb3351298c413394350520f150c97a4c00
Ben Hale <bhale@pivotal.io>
2017-02-16 15:28:56 -0800

    Move SLF4J over to Maven Central Resource

--- slf4j-api.sh
+++ slf4j-api.sh
@@ -4,28 +4,15 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo $(maven_central_uri 'org.slf4j' 'slf4j-api' $DOWNLOAD_VERSION)
-}
-
 upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/slf4j-api/slf4j-api-$UPLOAD_VERSION.jar"
+  echo "/slf4j-api/slf4j-api-$VERSION.jar"
 }
 
-DOWNLOAD_URI=$(download_uri)
+VERSION=$(cat slf4j-api-archives/version)
+
 UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/slf4j-api/index.yml"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "slf4j-api-archives/slfj4-api-$VERSION.jar" $UPLOAD_PATH
+update_index $INDEX_PATH $ERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
--- slf4j-jdk14.sh
+++ slf4j-jdk14.sh
@@ -4,28 +4,15 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo $(maven_central_uri 'org.slf4j' 'slf4j-jdk14' $DOWNLOAD_VERSION)
-}
-
 upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/slf4j-jdk14/slf4j-jdk14-$UPLOAD_VERSION.jar"
+  echo "/slf4j-jdk14/slf4j-jdk14-$VERSION.jar"
 }
 
-DOWNLOAD_URI=$(download_uri)
+VERSION=$(cat slf4j-jdk14-archives/version)
+
 UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/slf4j-jdk14/index.yml"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "slf4j-jdk14-archives/slfj4-jdk14-$VERSION.jar" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
