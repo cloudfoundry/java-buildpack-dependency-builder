4c044d717e3e922cb06f01620ced924c648cef90
Ben Hale <bhale@pivotal.io>
2017-02-21 13:47:54 -0800

    Convert tomcat-lifecycle-support to use maven-resource

--- tomcat-lifecycle-support.sh
+++ tomcat-lifecycle-support.sh
@@ -4,28 +4,15 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo $(spring_release_uri 'org.cloudfoundry' 'tomcat-lifecycle-support' $DOWNLOAD_VERSION)
-}
-
 upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/tomcat-lifecycle-support/tomcat-lifecycle-support-$UPLOAD_VERSION.jar"
+  echo "/tomcat-lifecycle-support/tomcat-lifecycle-support-$VERSION.jar"
 }
 
-DOWNLOAD_URI=$(download_uri)
+VERSION=$(cat tomcat-lifecycle-support-archives/version)
+
 UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/tomcat-lifecycle-support/index.yml"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "tomcat-lifecycle-support-archives/tomcat-lifecycle-support-*.jar" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
