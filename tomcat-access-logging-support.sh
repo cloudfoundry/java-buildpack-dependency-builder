a1b86f4f208e144e29962bb07dc874fe9f0a49f9
Ben Hale <bhale@pivotal.io>
2017-02-21 13:39:13 -0800

    Convert tomcat-access-logging-support to use maven-resource

--- tomcat-access-logging-support.sh
+++ tomcat-access-logging-support.sh
@@ -4,28 +4,15 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo $(spring_release_uri 'org.cloudfoundry' 'tomcat-access-logging-support' $DOWNLOAD_VERSION)
-}
-
 upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/tomcat-access-logging-support/tomcat-access-logging-support-$UPLOAD_VERSION.jar"
+  echo "/tomcat-access-logging-support/tomcat-access-logging-support-$VERSION.jar"
 }
 
-DOWNLOAD_URI=$(download_uri)
+VERSION=$(cat tomcat-access-logging-support-archives/version)
+
 UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/tomcat-access-logging-support/index.yml"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "tomcat-access-logging-support-archives/tomcat-access-logging-support-*.jar" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
