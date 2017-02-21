db1a3927a1846a0923cde07e4ef4d0127cc63574
Ben Hale <bhale@pivotal.io>
2017-02-21 13:15:06 -0800

    Convery container-certificate-trust-store to use maven-resource

--- container-certificate-trust-store.sh
+++ container-certificate-trust-store.sh
@@ -4,28 +4,14 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo $(spring_release_uri 'org.cloudfoundry' 'java-buildpack-container-certificate-trust-store' $DOWNLOAD_VERSION)
-}
-
 upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/container-certificate-trust-store/container-certificate-trust-store-$UPLOAD_VERSION.jar"
+  echo "/container-certificate-trust-store/container-certificate-trust-store-$VERSION.jar"
 }
 
 DOWNLOAD_URI=$(download_uri)
 UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/container-certificate-trust-store/index.yml"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "container-certificate-trust-store-archives/java-buildpack-container-certificate-trust-store-*.jar" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
