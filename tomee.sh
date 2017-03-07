1194bcaaa63632f85b93cae2dda858ebd837a117
Ben Hale <bhale@pivotal.io>
2017-03-07 14:01:59 -0800

    Convert tomee to use maven-resource

---
+++ tomee.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+source $(dirname "$0")/common.sh
+
+upload_path() {
+  echo "/tomee/tomee-$VERSION.tar.gz"
+}
+
+VERSION=$(cat tomee-archives/version)
+
+UPLOAD_PATH=$(upload_path)
+INDEX_PATH="/tomee/index.yml"
+
+transfer_to_s3 "tomee-archives/apache-tomee-*-webprofile.tar.gz" $UPLOAD_PATH
+update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
