467cd75f6a2d0be7ba95e957de3f84dbe56ba82a
Ben Hale <bhale@pivotal.io>
2017-03-07 13:46:42 -0800

    Convert groovy to use maven-resource

--- groovy.sh
+++ groovy.sh
@@ -4,28 +4,15 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "http://dl.bintray.com/groovy/maven/apache-groovy-binary-$DOWNLOAD_VERSION.zip"
-}
-
 upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/groovy/groovy-$UPLOAD_VERSION.zip"
+  echo "/groovy/groovy-$VERSION.zip"
 }
 
-DOWNLOAD_URI=$(download_uri)
+VERSION=$(cat groovy-archives/version)
+
 UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/groovy/index.yml"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "groovy-archives/groovy-binary-*.zip" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
