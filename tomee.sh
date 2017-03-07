76c6f54a09a7f94d58c79fcf330e8b5f11916999
Ben Hale <bhale@pivotal.io>
2017-03-07 14:04:39 -0800

    Polishing

--- tomee.sh
+++ tomee.sh
@@ -14,5 +14,5 @@ UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/tomee/index.yml"
 
 transfer_to_s3 "tomee-archives/apache-tomee-*-webprofile.tar.gz" $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
