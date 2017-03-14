1655f5465b20d7b27ece7e6bfdae03a1841face3
Ben Hale <bhale@pivotal.io>
2017-03-14 11:51:11 -0700

    Create docker image directories

--- resources/ci/build.sh
+++ resources/ci/build.sh
@@ -3,4 +3,10 @@
 set -e
 
 cd java-buildpack-dependency-builder/resources
-./mvnw package
+./mvnw -q package
+cd -
+
+cp -r java-buildpack-dependency-builder/resources/artifactory-resource-docker-image/* artifactory-resource-docker-image
+cp -r java-buildpack-dependency-builder/resources/maven-resource-docker-image/* maven-resource-docker-image
+cp -r java-buildpack-dependency-builder/resources/pivotal-network-resource-docker-image/* pivotal-network-resource-docker-image
+cp -r java-buildpack-dependency-builder/resources/tomcat-resource-docker-image/* tomcat-resource-docker-image
