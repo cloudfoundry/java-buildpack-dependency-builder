775fa2089716de8369c61c7da4b8e051c5521bf3
Ben Hale <bhale@pivotal.io>
2017-03-14 17:27:37 -0700

    Update Wildfly to use wildfly-resource

--- wildfly.sh
+++ wildfly.sh
@@ -4,28 +4,11 @@ set -e -o pipefail
 
 source $(dirname "$0")/common.sh
 
-download_uri() {
-  if [[ -z "$DOWNLOAD_VERSION" ]]; then
-    echo "DOWNLOAD_VERSION must be set" >&2
-    exit 1
-  fi
+VERSION=$(cat wildfly-archives/version)
 
-  echo "http://download.jboss.org/wildfly/$DOWNLOAD_VERSION/wildfly-$DOWNLOAD_VERSION.tar.gz"
-}
-
-upload_path() {
-  if [[ -z "$UPLOAD_VERSION" ]]; then
-    echo "UPLOAD_VERSION must be set" >&2
-    exit 1
-  fi
-
-  echo "/wildfly/wildfly-$UPLOAD_VERSION.tar.gz"
-}
-
-DOWNLOAD_URI=$(download_uri)
-UPLOAD_PATH=$(upload_path)
 INDEX_PATH="/wildfly/index.yml"
+UPLOAD_PATH="/wildfly/wildfly-$VERSION.tar.gz"
 
-transfer_direct $DOWNLOAD_URI $UPLOAD_PATH
-update_index $INDEX_PATH $UPLOAD_VERSION $UPLOAD_PATH
+transfer_to_s3 "wildfly-archives/wildfly-*.tar.gz" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
