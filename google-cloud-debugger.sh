682cd21ded10fb5dcd9b6b027d081fa6d57f029b
Ben Hale <bhale@pivotal.io>
2017-03-17 13:03:49 -0700

    Add Google Cloud Debugger
    This change adds dependency builder infrastructure for building the Google
    Cloud Debugger Java Agent.

---
+++ google-cloud-debugger.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+source $(dirname "$0")/common.sh
+
+cd cloud-debug-java
+chmod +x build.sh
+./build.sh
+
+VERSION=$(git describe --tags)
+VERSION=${VERSION##v}
+
+INDEX_PATH="/google-cloud-debugger/index.yml"
+UPLOAD_PATH="/google-cloud-debugger/google-cloud-debugger-$VERSION.tar.gz"
+
+transfer_to_s3 "cdbg_java_agent_gce.tar.gz" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
