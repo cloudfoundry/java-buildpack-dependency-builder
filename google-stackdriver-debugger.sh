e1facd2abc2867434e74553736d1503070c45ba4
Ben Hale <bhale@pivotal.io>
2017-04-05 09:13:40 -0700

    Polishing Up Stackdriver Debugger

--- google-stackdriver-debugger.sh
+++ google-stackdriver-debugger.sh
@@ -11,11 +11,11 @@ chmod +x build.sh
 ./build.sh
 
 VERSION=$(git describe --tags)
-VERSION=${VERSION##v}
+VERSION="${VERSION##v}.0"
 
 INDEX_PATH="/google-stackdriver-debugger/$PLATFORM/x86_64/index.yml"
 UPLOAD_PATH="/google-stackdriver-debugger/$PLATFORM/x86_64/google-stackdriver-debugger-$VERSION.tar.gz"
 
-transfer_to_s3 "cdbg_java_agent_gce.tar.gz" $UPLOAD_PATH
+transfer_to_s3 "cdbg_java_agent_service_account.tar.gz" $UPLOAD_PATH
 update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
