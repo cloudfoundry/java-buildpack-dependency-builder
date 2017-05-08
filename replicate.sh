1a414e34ccd3ee14bf65eba09b7c4d08e2120f88
Ben Hale <bhale@pivotal.io>
2017-05-08 12:13:35 -0700

    Remove Non-Open Source from Replication
    Previously, all of the dependencies required for the Java Buildpack were
    downloaded as part of the replication script.  Given recent guidance from the
    Cloud Foundry Foundation, dependencies that are not backed by pure open source
    can be integrated but should not be available from CFF domains.  This is a
    step towards that guidance removing non-open source dependencies from the
    replications script.

--- replicate.sh
+++ replicate.sh
@@ -21,14 +21,11 @@ aws s3 sync "s3://download.pivotal.io" "$DESTINATION" --no-sign-request --exclud
   --include "jvmkill/*" \
   --include "mariadb-jdbc/*" \
   --include "memory-calculator/*" \
-  --include "new-relic/*" \
   --include "openjdk/*" \
-  --include "openjdk-jdk/*" \
   --include "play-jpa-plugin/*" \
   --include "postgresql-jdbc/*" \
   --include "redis-store/*" \
   --include "spring-boot-cli/*" \
-  --include "tc-server/*" \
   --include "tomcat/*" \
   --include "tomcat-access-logging-support/*" \
   --include "tomcat-lifecycle-support/*" \
@@ -36,9 +33,9 @@ aws s3 sync "s3://download.pivotal.io" "$DESTINATION" --no-sign-request --exclud
   --include "tomee/*" \
   --include "tomee-resource-configuration/*" \
   --include "wildfly/*" \
-  --include "your-kit/*" \
   --exclude "*/centos6/*" \
   --exclude "*/lucid/*" \
+  --exclude "*/mountainlion/*" \
   --exclude "*/precise/*"
 
 find "$DESTINATION" -name "index.yml" | xargs sed -ie "s|https://java-buildpack.cloudfoundry.org|$BASE_URI|g"
