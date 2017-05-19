acc7379b8bb1d0c6699ac1935edcf081044c4bd1
Ben Hale <bhale@pivotal.io>
2017-05-19 14:45:28 -0700

    Rename security provider to container security provider

---
+++ container-security-provider.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+source $(dirname "$0")/common.sh
+
+VERSION=$(cat container-security-provider-archives/version)
+
+INDEX_PATH="/container-security-provider/index.yml"
+UPLOAD_PATH="/container-security-provider/container-security-provider-$VERSION.jar"
+
+transfer_to_s3 "container-security-provider-archives/java-buildpack-container-security-provider-*.jar" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
