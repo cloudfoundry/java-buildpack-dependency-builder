54ce9db5a8f08157956315d43e15eb5618556f0d
Ben Hale <bhale@pivotal.io>
2017-11-07 13:02:24 -0800

    New Jar Name

--- auto-reconfiguration.sh
+++ auto-reconfiguration.sh
@@ -9,6 +9,6 @@ VERSION=$(cat auto-reconfiguration-archives/version)
 INDEX_PATH="/auto-reconfiguration/index.yml"
 UPLOAD_PATH="/auto-reconfiguration/auto-reconfiguration-$VERSION.jar"
 
-transfer_to_s3 "auto-reconfiguration-archives/auto-reconfiguration-*.jar" $UPLOAD_PATH
+transfer_to_s3 "auto-reconfiguration-archives/java-buildpack-auto-reconfiguration-*.jar" $UPLOAD_PATH
 update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
