cf4aeed38051cfdbcf3cf8499cee402d082516e5
Ben Hale <bhale@pivotal.io>
2018-02-26 12:29:53 -0800

    Polishing

--- sky-walking.sh
+++ sky-walking.sh
@@ -9,6 +9,6 @@ VERSION=$(cat sky-walking-archives/version)
 INDEX_PATH="/sky-walking/index.yml"
 UPLOAD_PATH="/sky-walking/sky-walking-$VERSION.tar.gz"
 
-transfer_to_s3 "sky-walking-archives/sky-walking-agent.tar.gz" $UPLOAD_PATH
+transfer_to_s3 "sky-walking-archives/skywalking-agent.tar.gz" $UPLOAD_PATH
 update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
