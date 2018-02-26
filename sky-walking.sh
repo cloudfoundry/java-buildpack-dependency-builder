20047a6609b78fa0f095de71ef5f4ffac9527099
Ben Hale <bhale@pivotal.io>
2018-02-26 12:23:52 -0800

    Rename skywalking to sky-walking

---
+++ sky-walking.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+source $(dirname "$0")/common.sh
+
+VERSION=$(cat sky-walking-archives/version)
+
+INDEX_PATH="/sky-walking/index.yml"
+UPLOAD_PATH="/sky-walking/sky-walking-$VERSION.tar.gz"
+
+transfer_to_s3 "sky-walking-archives/sky-walking-agent.tar.gz" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
