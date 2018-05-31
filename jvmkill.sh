24ad55f9957ec586afcee36bcdb823a22d3b3363
Ben Hale <bhale@pivotal.io>
2018-05-31 13:51:00 -0700

    jvmkill bionic

--- jvmkill.sh
+++ jvmkill.sh
@@ -6,13 +6,19 @@ source $(dirname "$0")/common.sh
 
 VERSION=$(cat jvmkill-archives/version)
 
+
+INDEX_PATH_BIONIC="/jvmkill/bionic/x86_64/index.yml"
 INDEX_PATH_MOUNTAINLION="/jvmkill/mountainlion/x86_64/index.yml"
 INDEX_PATH_TRUSTY="/jvmkill/trusty/x86_64/index.yml"
+UPLOAD_PATH_BIONIC="/jvmkill/bionic/x86_64/jvmkill-$VERSION.so"
 UPLOAD_PATH_MOUNTAINLION="/jvmkill/mountainlion/x86_64/jvmkill-$VERSION.so"
 UPLOAD_PATH_TRUSTY="/jvmkill/trusty/x86_64/jvmkill-$VERSION.so"
 
+
+transfer_to_s3 "jvmkill-archives/jvmkill-*-bionic.so" $UPLOAD_PATH_BIONIC
 transfer_to_s3 "jvmkill-archives/jvmkill-*-darwin.so" $UPLOAD_PATH_MOUNTAINLION
 transfer_to_s3 "jvmkill-archives/jvmkill-*-trusty.so" $UPLOAD_PATH_TRUSTY
+update_index $INDEX_PATH_BIONIC $VERSION $UPLOAD_PATH_BIONIC
 update_index $INDEX_PATH_MOUNTAINLION $VERSION $UPLOAD_PATH_MOUNTAINLION
 update_index $INDEX_PATH_TRUSTY $VERSION $UPLOAD_PATH_TRUSTY
-invalidate_cache $INDEX_PATH_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION $INDEX_PATH_TRUSTY $UPLOAD_PATH_TRUSTY
+invalidate_cache $INDEX_PATH_BIONIC $UPLOAD_PATH_BIONIC $INDEX_PATH_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION $INDEX_PATH_TRUSTY $UPLOAD_PATH_TRUSTY
