5c1521b98e92c9080c64ab494f0cbf307d555046
Ben Hale <bhale@pivotal.io>
2018-06-07 11:56:21 -0700

    Google Stackdriver Profiler
    This change adds builders for the Google Stackdriver Profiler.

---
+++ google-stackdriver-profiler.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+source $(dirname "$0")/common.sh
+
+PATH=/usr/local/bin:$PATH
+
+cd cloud-profile-java
+chmod +x build.sh
+./build.sh
+
+VERSION=$(git describe --tags)
+VERSION="${VERSION##v}.0"
+
+INDEX_PATH="/google-stackdriver-profiler/$PLATFORM/x86_64/index.yml"
+UPLOAD_PATH="/google-stackdriver-profiler/$PLATFORM/x86_64/google-stackdriver-profiler-$VERSION.tar.gz"
+
+transfer_to_s3 "cdpf_java_agent_service_account.tar.gz" $UPLOAD_PATH
+update_index $INDEX_PATH $VERSION $UPLOAD_PATH
+invalidate_cache $INDEX_PATH $UPLOAD_PATH
