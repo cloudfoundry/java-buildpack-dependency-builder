e59422aca34d3dd1e6db1a5b8d5f35b21a8cecc2
Ben Hale <bhale@pivotal.io>
2018-06-08 12:39:17 -0700

    Add HTTP Resource to CI

--- resources/ci/build.sh
+++ resources/ci/build.sh
@@ -7,6 +7,7 @@ cd java-buildpack-dependency-builder/resources
 cd -
 
 cp -r java-buildpack-dependency-builder/resources/artifactory-resource-docker-image/* artifactory-resource-docker-image
+cp -r java-buildpack-dependency-builder/resources/http-resource-docker-image/* http-resource-docker-image
 cp -r java-buildpack-dependency-builder/resources/java-resource-docker-image/* java-resource-docker-image
 cp -r java-buildpack-dependency-builder/resources/maven-resource-docker-image/* maven-resource-docker-image
 cp -r java-buildpack-dependency-builder/resources/pivotal-network-resource-docker-image/* pivotal-network-resource-docker-image
