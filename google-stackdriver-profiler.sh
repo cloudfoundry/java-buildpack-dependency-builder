3a692a72e1bff3d0cc252e2904b8847f2127dea3
Ben Hale <bhale@pivotal.io>
2018-07-24 14:20:25 -0700

    More Stackdriver Profiler Attempts

--- google-stackdriver-profiler.sh
+++ google-stackdriver-profiler.sh
@@ -7,15 +7,17 @@ source $(dirname "$0")/common.sh
 PATH=/usr/local/bin:$PATH
 
 cd cloud-profiler-java
-chmod +x build.sh
-./build.sh
+make all
+tar zcf profiler_java_agent.tar.gz \
+    -C build/.out \
+    NOTICES profiler_java_agent.so
 
 VERSION=$(git describe --tags)
-VERSION="${VERSION##v}.0"
+VERSION="${VERSION##v}"
 
 INDEX_PATH="/google-stackdriver-profiler/$PLATFORM/x86_64/index.yml"
 UPLOAD_PATH="/google-stackdriver-profiler/$PLATFORM/x86_64/google-stackdriver-profiler-$VERSION.tar.gz"
 
-transfer_to_s3 "cdpf_java_agent_service_account.tar.gz" $UPLOAD_PATH
+transfer_to_s3 "profiler_java_agent.tar.gz" $UPLOAD_PATH
 update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
