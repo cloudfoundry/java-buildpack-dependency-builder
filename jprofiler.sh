a11344ac68ae8a536a09d4fe08a8360b4edc686d
Ben Hale <bhale@pivotal.io>
2018-08-06 14:33:03 -0700

    Polishing

--- jprofiler.sh
+++ jprofiler.sh
@@ -11,7 +11,7 @@ INDEX_PATH_TRUSTY="/jprofiler/trusty/x86_64/index.yml"
 UPLOAD_PATH_BIONIC="/jprofiler/bionic/x86_64/jprofiler-$VERSION.so"
 UPLOAD_PATH_TRUSTY="/jprofiler/trusty/x86_64/jprofiler-$VERSION.so"
 
-tar xzf "jprofiler-archives/jprofiler_linux_*.tar.gz"
+tar xzf jprofiler-archives/jprofiler_linux_*.tar.gz
 
 transfer_to_s3 'jprofiler*/bin/linux-x64/libjprofilerti.so' $UPLOAD_PATH_TRUSTY
 transfer_to_s3 'jprofiler*/bin/linux-x64/libjprofilerti.so' $UPLOAD_PATH_BIONIC
