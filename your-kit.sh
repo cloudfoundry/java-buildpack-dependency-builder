15eec9a8aeba55d2e12ad765446653985650bb45
Ben Hale <bhale@pivotal.io>
2019-04-02 12:07:10 -0700

    Script Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- your-kit.sh
+++ your-kit.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 VERSION=$(cat your-kit-archives/version)
 
 unzip -qq "your-kit-archives/YourKit-JavaProfiler-*.zip"
+FILE="YourKit-JavaProfiler-*/bin/linux-x86-64/libyjpagent.so"
 
-cp 'YourKit-JavaProfiler-*/bin/linux-x86-64/libyjpagent.so' "repository/your-kit-$VERSION.so"
+cp "${FILE}" "repository/your-kit-${VERSION}.so"
 cp your-kit-archives/version repository/version
