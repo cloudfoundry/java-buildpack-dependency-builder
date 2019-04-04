4cbbcb324e41b8bafbeea182ca852fa4db2b0668
Ben Hale <bhale@pivotal.io>
2019-04-04 09:42:39 -0400

    Container Customizer Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- container-customizer.sh
+++ container-customizer.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat container-customizer-archives/version)
 
-INDEX_PATH="/container-customizer/index.yml"
-UPLOAD_PATH="/container-customizer/container-customizer-$VERSION.jar"
-
-transfer_to_s3 "container-customizer-archives/java-buildpack-container-customizer-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp container-customizer-archives/java-buildpack-container-customizer-*.jar repository/container-customizer-$VERSION.jar
+cp container-customizer-archives/version repository/version
