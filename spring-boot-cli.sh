e11bdd000cade5585e0b32195b982e37225f01cf
Ben Hale <bhale@pivotal.io>
2019-04-04 10:16:02 -0400

    Spring Boot CLI Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- spring-boot-cli.sh
+++ spring-boot-cli.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat spring-boot-cli-archives/version)
 
-INDEX_PATH="/spring-boot-cli/index.yml"
-UPLOAD_PATH="/spring-boot-cli/spring-boot-cli-$VERSION.tar.gz"
-
-transfer_to_s3 "spring-boot-cli-archives/spring-boot-cli-*-bin.tar.gz" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp spring-boot-cli-archives/spring-boot-cli-*-bin.tar.gz repository/spring-boot-cli-$VERSION.tar.gz
+cp spring-boot-cli-archives/version repository/version
