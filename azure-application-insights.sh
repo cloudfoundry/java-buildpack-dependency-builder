bd1800879575d2f94b38d2c06c96a959a245d1b2
Ben Hale <bhale@pivotal.io>
2019-04-04 19:32:02 -0400

    Azure Application Insights Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- azure-application-insights.sh
+++ azure-application-insights.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat azure-application-insights-archives/version)
 
-INDEX_PATH="/azure-application-insights/index.yml"
-UPLOAD_PATH="/azure-application-insights/azure-application-insights-$VERSION.jar"
-
-transfer_to_s3 "azure-application-insights-archives/applicationinsights-agent-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp azure-application-insights-archives/applicationinsights-agent-*.jar repository/azure-application-insights-$VERSION.jar
+cp azure-application-insights-archives/version repository/version
