29705a529858bec037e614aa047c1e9ea087e923
Ben Hale <bhale@pivotal.io>
2019-04-04 10:29:00 -0400

    Tomcat Logging Support Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tomcat-logging-support.sh
+++ tomcat-logging-support.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat tomcat-logging-support-archives/version)
 
-INDEX_PATH="/tomcat-logging-support/index.yml"
-UPLOAD_PATH="/tomcat-logging-support/tomcat-logging-support-$VERSION.jar"
-
-transfer_to_s3 "tomcat-logging-support-archives/tomcat-logging-support-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp tomcat-logging-support-archives/tomcat-logging-support-archives/tomcat-logging-support-*.jar repository/tomcat-logging-support-$VERSION.jar
+cp tomcat-logging-support-archives/version repository/version
