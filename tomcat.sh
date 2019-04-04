1ed89ef8c6bbd2c6b31937bc3352fea15c8798ab
Ben Hale <bhale@pivotal.io>
2019-04-04 06:31:59 -0400

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tomcat.sh
+++ tomcat.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 VERSION=$(cat tomcat-archives/version)
 
 cp tomcat-archives/apache-tomcat-*.tar.gz repository/tomcat-$VERSION.tar.gz
-cp tomcat-archives/version repository-version
+cp tomcat-archives/version repository/version
