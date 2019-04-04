da5923d95efae3e45d8b97f66d9b7d0cdf807be0
Ben Hale <bhale@pivotal.io>
2019-04-04 10:30:31 -0400

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tomcat-lifecycle-support.sh
+++ tomcat-lifecycle-support.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 VERSION=$(cat tomcat-lifecycle-support-archives/version)
 
-cp tomcat-lifecycle-support-archives/tomcat-lifecycle-support-archives/tomcat-lifecycle-support-*.jar repository/tomcat-lifecycle-support-$VERSION.jar
+cp tomcat-lifecycle-support-archives/tomcat-lifecycle-support-*.jar repository/tomcat-lifecycle-support-$VERSION.jar
 cp tomcat-lifecycle-support-archives/version repository/version
--- tomcat-logging-support.sh
+++ tomcat-logging-support.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 VERSION=$(cat tomcat-logging-support-archives/version)
 
-cp tomcat-logging-support-archives/tomcat-logging-support-archives/tomcat-logging-support-*.jar repository/tomcat-logging-support-$VERSION.jar
+cp tomcat-logging-support-archives/tomcat-logging-support-*.jar repository/tomcat-logging-support-$VERSION.jar
 cp tomcat-logging-support-archives/version repository/version
