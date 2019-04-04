ae1576050bf21389f735a08e2e6c35a7d23dc100
Ben Hale <bhale@pivotal.io>
2019-04-04 08:44:33 -0400

    Client Certificate Mapper Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- client-certificate-mapper.sh
+++ client-certificate-mapper.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat client-certificate-mapper-archives/version)
 
-INDEX_PATH="/client-certificate-mapper/index.yml"
-UPLOAD_PATH="/client-certificate-mapper/client-certificate-mapper-$VERSION.jar"
-
-transfer_to_s3 "client-certificate-mapper-archives/java-buildpack-client-certificate-mapper-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp client-certificate-mapper-archives/java-buildpack-client-certificate-mapper-*.jar repository/client-certificate-mapper-$VERSION.jar
+cp client-certificate-mapper-archives/version repository/version
