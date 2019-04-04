07ffe10b56a8ef03d65cbe98a1d05e50547612b0
Ben Hale <bhale@pivotal.io>
2019-04-04 10:35:03 -0400

    TomEE Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tomee.sh
+++ tomee.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat tomee-archives/version)
 
-INDEX_PATH="/tomee/index.yml"
-UPLOAD_PATH="/tomee/tomee-$VERSION.tar.gz"
-
-transfer_to_s3 "tomee-archives/apache-tomee-*-webprofile.tar.gz" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp tomee-archives/apache-tomee-*-webprofile.tar.gz repository/tomee-$VERSION.jar
+cp tomee-archives/version repository/version
