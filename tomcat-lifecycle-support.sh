fbdab743641fe391a2569e55c7de199eb6d61bbd
Ben Hale <bhale@pivotal.io>
2019-04-04 10:28:38 -0400

    Tomcat Lifecycle Support Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tomcat-lifecycle-support.sh
+++ tomcat-lifecycle-support.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat tomcat-lifecycle-support-archives/version)
 
-INDEX_PATH="/tomcat-lifecycle-support/index.yml"
-UPLOAD_PATH="/tomcat-lifecycle-support/tomcat-lifecycle-support-$VERSION.jar"
-
-transfer_to_s3 "tomcat-lifecycle-support-archives/tomcat-lifecycle-support-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp tomcat-lifecycle-support-archives/tomcat-lifecycle-support-archives/tomcat-lifecycle-support-*.jar repository/tomcat-lifecycle-support-$VERSION.jar
+cp tomcat-lifecycle-support-archives/version repository/version
