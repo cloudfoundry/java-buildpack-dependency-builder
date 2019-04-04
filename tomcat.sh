c9bf4121da99e0f624487a937dbb1ab2d253fec6
Ben Hale <bhale@pivotal.io>
2019-04-04 06:24:50 -0400

    Tomcat Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tomcat.sh
+++ tomcat.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat tomcat-archives/version)
 
-INDEX_PATH="/tomcat/index.yml"
-UPLOAD_PATH="/tomcat/tomcat-$VERSION.tar.gz"
-
-transfer_to_s3 "tomcat-archives/apache-tomcat-*.tar.gz" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp tomcat-archives/apache-tomcat-*.tar.gz repository/tomcat-$VERSION.tar.gz
+cp tomcat-archives/version repository-version
