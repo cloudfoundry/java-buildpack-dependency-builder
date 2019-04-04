94ca69fa8d15292b1ef42e46937953850aeb6920
Ben Hale <bhale@pivotal.io>
2019-04-04 08:37:32 -0400

    Stick to semver
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- auto-reconfiguration.sh
+++ auto-reconfiguration.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-source $(dirname "$0")/common.sh
-
-VERSION=$(semver_to_repository_version $(cat auto-reconfiguration-archives/version))
+VERSION=$(cat auto-reconfiguration-archives/version)
 
 cp auto-reconfiguration-archives/java-buildpack-auto-reconfiguration-*.jar repository/auto-reconfiguration-$VERSION.jar
 cp auto-reconfiguration-archives/version repository/version
