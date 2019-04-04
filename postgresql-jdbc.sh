2ff990440132d15ac004057437c2d0a747c38ef4
Ben Hale <bhale@pivotal.io>
2019-04-04 10:10:14 -0400

    PostgreSQL JDBC Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- postgresql-jdbc.sh
+++ postgresql-jdbc.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat postgresql-jdbc-archives/version)
 
-INDEX_PATH="/postgresql-jdbc/index.yml"
-UPLOAD_PATH="/postgresql-jdbc/postgresql-jdbc-$VERSION.jar"
-
-transfer_to_s3 "postgresql-jdbc-archives/postgresql-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp postgresql-jdbc-archives/postgresql-*.jar repository/postgresql-jdbc-$VERSION.jar
+cp postgresql-jdbc-archives/version repository/version
