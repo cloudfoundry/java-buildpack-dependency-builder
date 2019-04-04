33147c7fadf1f0fac37b0f5e930fcdeab33bfe4d
Ben Hale <bhale@pivotal.io>
2019-04-04 10:05:33 -0400

    New Relic Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- new-relic.sh
+++ new-relic.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat new-relic-archives/version)
 
-INDEX_PATH="/new-relic/index.yml"
-UPLOAD_PATH="/new-relic/new-relic-$VERSION.jar"
-
-transfer_to_s3 "new-relic-archives/newrelic-agent-*.jar" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp new-relic-archives/newrelic-agent-*.jar repository/new-relic-$VERSION.jar
+cp new-relic-archives/version repository/version
