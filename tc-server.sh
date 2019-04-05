2572b1c79e8fddef5d79166b68945edbc8f00cfd
Ben Hale <bhale@pivotal.io>
2019-04-05 11:03:25 -0400

    tc Server Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- tc-server.sh
+++ tc-server.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
+set -euo pipefail
 
-source $(dirname "$0")/common.sh
+VERSION=$(cat tc-server-archives/metadata.json | jq -r '.Release.Version')
 
-VERSION=$(cat tc-server-archives/version)
-
-INDEX_PATH="/tc-server/index.yml"
-UPLOAD_PATH="/tc-server/tc-server-$VERSION.tar.gz"
-
-transfer_to_s3 "tc-server-archives/pivotal-tc-server-standard-*.tar.gz" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp tc-server-archives/pivotal-tc-server-standard-*.tar.gz repository/tc-server-$VERSION.tar.gz
+echo $VERSION > repository/version
