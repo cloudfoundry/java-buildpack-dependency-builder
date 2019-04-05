2cbc7386366daec5bd45e24f7f6c1409d6bed63a
Ben Hale <bhale@pivotal.io>
2019-04-05 09:31:08 -0400

    WildFly Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- wildfly.sh
+++ wildfly.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat wildfly-archives/version)
 
-INDEX_PATH="/wildfly/index.yml"
-UPLOAD_PATH="/wildfly/wildfly-$VERSION.tar.gz"
-
-transfer_to_s3 "wildfly-archives/wildfly-*.tar.gz" $UPLOAD_PATH
-update_index $INDEX_PATH $VERSION $UPLOAD_PATH
-invalidate_cache $INDEX_PATH $UPLOAD_PATH
+cp wildfly-archives/wildfly-*.tar.gz repository/wildfly-$VERSION.tar.gz
+cp wildfly-archives/version repository/version
