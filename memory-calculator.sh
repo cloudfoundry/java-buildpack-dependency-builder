64a8b4bfa4842fc7d47d4c136940b61348431aac
Ben Hale <bhale@pivotal.io>
2019-04-05 13:59:22 -0700

    Memory Calculator Repository Publishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- memory-calculator.sh
+++ memory-calculator.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 
-set -e -u -o pipefail
-
-source $(dirname "$0")/common.sh
+set -euo pipefail
 
 VERSION=$(cat memory-calculator-archives/version)
 
-INDEX_PATH_BIONIC="/memory-calculator/bionic/x86_64/index.yml"
-INDEX_PATH_MOUNTAINLION="/memory-calculator/mountainlion/x86_64/index.yml"
-INDEX_PATH_TRUSTY="/memory-calculator/trusty/x86_64/index.yml"
-UPLOAD_PATH_BIONIC="/memory-calculator/bionic/x86_64/memory-calculator-$VERSION.tar.gz"
-UPLOAD_PATH_MOUNTAINLION="/memory-calculator/mountainlion/x86_64/memory-calculator-$VERSION.tar.gz"
-UPLOAD_PATH_TRUSTY="/memory-calculator/trusty/x86_64/memory-calculator-$VERSION.tar.gz"
-
-transfer_to_s3 "memory-calculator-archives/java-buildpack-memory-calculator-*-linux.tar.gz" $UPLOAD_PATH_BIONIC
-transfer_to_s3 "memory-calculator-archives/java-buildpack-memory-calculator-*-darwin.tar.gz" $UPLOAD_PATH_MOUNTAINLION
-transfer_to_s3 "memory-calculator-archives/java-buildpack-memory-calculator-*-linux.tar.gz" $UPLOAD_PATH_TRUSTY
-update_index $INDEX_PATH_BIONIC $VERSION $UPLOAD_PATH_BIONIC
-update_index $INDEX_PATH_MOUNTAINLION $VERSION $UPLOAD_PATH_MOUNTAINLION
-update_index $INDEX_PATH_TRUSTY $VERSION $UPLOAD_PATH_TRUSTY
-invalidate_cache $INDEX_PATH_BIONIC $UPLOAD_PATH_BIONIC $INDEX_PATH_MOUNTAINLION $UPLOAD_PATH_MOUNTAINLION $INDEX_PATH_TRUSTY $UPLOAD_PATH_TRUSTY
+cp memory-calculator-archives/java-buildpack-memory-calculator-*.tar.gz repository/memory-calculator-$VERSION.tar.gz
+cp memory-calculator-archives/version repository/version
