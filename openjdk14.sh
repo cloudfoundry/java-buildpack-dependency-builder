6c7de596a52dca03b7161ff071b975f39cbfeef4
Ben Hale <bhale@pivotal.io>
2020-04-15 15:21:06 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- openjdk14.sh
+++ openjdk14.sh
@@ -7,7 +7,7 @@ cp jre-14/bellsoft-jre*.tar.gz images
 
 PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)"
 if [[ $(cat jdk-14/version) =~ ${PATTERN} ]]; then
-  echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}_${BASH_REMATCH[4]}" > images/version
+  echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}-${BASH_REMATCH[4]}" > images/version
 else
   echo "version is not semver" 1>&2
   exit 1
