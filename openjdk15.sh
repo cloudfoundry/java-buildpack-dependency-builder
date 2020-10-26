b254e815632dc095193f000cac75a8f5126ee4e5
Ben Hale <bhale@vmware.com>
2020-10-26 08:38:41 -0700

    Java 15
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- openjdk15.sh
+++ openjdk15.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-cp jdk-14/bellsoft-jdk*.tar.gz images
-cp jre-14/bellsoft-jre*.tar.gz images
+cp jdk-15/bellsoft-jdk*.tar.gz images
+cp jre-15/bellsoft-jre*.tar.gz images
 
 PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)"
-if [[ $(cat jdk-14/version) =~ ${PATTERN} ]]; then
+if [[ $(cat jdk-15/version) =~ ${PATTERN} ]]; then
   echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}-${BASH_REMATCH[4]}" > images/version
 else
   echo "version is not semver" 1>&2
