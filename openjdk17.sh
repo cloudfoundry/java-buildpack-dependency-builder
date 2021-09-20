94d288ffa585f713c939f9224e16db85bd70161b
Daniel Mikusa <dmikusa@vmware.com>
2021-09-20 13:32:51 -0400

    Add resources for Java 17
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

---
+++ openjdk17.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cp jdk-17/bellsoft-jdk*.tar.gz images
+cp jre-17/bellsoft-jre*.tar.gz images
+
+PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)"
+if [[ $(cat jdk-17/version) =~ ${PATTERN} ]]; then
+  echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}-${BASH_REMATCH[4]}" > images/version
+else
+  echo "version is not semver" 1>&2
+  exit 1
+fi
