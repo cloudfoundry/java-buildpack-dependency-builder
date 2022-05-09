b2d3abeef2502aced336c8750393c078c4a6d880
Daniel Mikusa <dmikusa@vmware.com>
2022-05-09 11:16:24 -0400

    Remove Redis store
    The Pivotal Redis store has been archived for a while. Given this we are replacing it with Redisson, which is maintained.
    
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

--- redis-store.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-VERSION=$(cat redis-store-archives/version)
-
-cp redis-store-archives/redis-store-*.jar repository/redis-store-$VERSION.jar
-cp redis-store-archives/version repository/version
