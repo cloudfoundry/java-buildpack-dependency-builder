87f032d771b0ce74ba6042489b529861e54d63ad
Daniel Mikusa <dmikusa@vmware.com>
2022-08-31 12:29:07 -0400

    Updates Google Stackdriver profiler to pull from Google's release bucket
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

--- google-stackdriver-profiler.sh
+++ google-stackdriver-profiler.sh
@@ -2,34 +2,6 @@
 
 set -euo pipefail
 
-SOURCE_DIRECTORY="cloud-profiler-java"
-
-build() {
-  pushd ${SOURCE_DIRECTORY} > /dev/null
-
-    PATH=/usr/local/bin:$PATH
-
-    make JAVA_PATH="/usr/lib/jvm/java-8-openjdk-amd64" all
-    tar zcf profiler_java_agent.tar.gz \
-      -C .out \
-      NOTICES profiler_java_agent.so
-
-  popd > /dev/null
-}
-
-version() {
-  pushd ${SOURCE_DIRECTORY} > /dev/null
-
-    local version=$(git describe --tags)
-    version="${version##v}"
-
-    echo ${version}
-
-  popd > /dev/null
-}
-
-VERSION=$(version)
-
-build
-cp ${SOURCE_DIRECTORY}/profiler_java_agent.tar.gz repository/google-stackdriver-profiler-$VERSION.tar.gz
-echo ${VERSION} > repository/version
+VERSION=$(find ./cloud-profiler-java -name "cloud-profiler-java-agent_*.tar.gz" -print0 | sed -z 's/.*cloud-profiler-java-agent_\(.*\).tar.gz/\1/')
+cp cloud-profiler-java/cloud-profiler-java-agent_*.tar.gz repository/google-stackdriver-profiler-$VERSION.tar.gz
+cp cloud-profiler-java/version repository/version
