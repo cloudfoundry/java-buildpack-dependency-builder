68eb50634788389c2bbfd4076ce50ec2a317a2d4
Anthony Dahanne <anthony.dahanne@gmail.com>
2023-04-11 14:44:42 -0400

    Remove Java 19, add 20

--- openjdk20.sh
+++ openjdk20.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-cp jdk-19/bellsoft-jdk*.tar.gz images
-cp jre-19/bellsoft-jre*.tar.gz images
+cp jdk-20/bellsoft-jdk*.tar.gz images
+cp jre-20/bellsoft-jre*.tar.gz images
 
 PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)"
-if [[ $(cat jdk-19/version) =~ ${PATTERN} ]]; then
+if [[ $(cat jdk-20/version) =~ ${PATTERN} ]]; then
   echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}-${BASH_REMATCH[4]}" > images/version
 else
   echo "version is not semver" 1>&2
