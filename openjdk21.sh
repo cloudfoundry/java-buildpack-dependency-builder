88910779eb92a1ae859554faf4d159441897022f
David O'Sullivan <davidos@vmware.com>
2023-09-22 10:11:44 +0100

    Adds JDK 21, removes 20

--- openjdk21.sh
+++ openjdk21.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-cp jdk-20/bellsoft-jdk*.tar.gz images
-cp jre-20/bellsoft-jre*.tar.gz images
+cp jdk-21/bellsoft-jdk*.tar.gz images
+cp jre-21/bellsoft-jre*.tar.gz images
 
 PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)"
-if [[ $(cat jdk-20/version) =~ ${PATTERN} ]]; then
+if [[ $(cat jdk-21/version) =~ ${PATTERN} ]]; then
   echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}-${BASH_REMATCH[4]}" > images/version
 else
   echo "version is not semver" 1>&2
