45641f957f14de4ace349484905208aa8379ac18
David O'Sullivan <31728678+pivotal-david-osullivan@users.noreply.github.com>
2024-03-06 09:31:01 +0000

    Update jprofiler.sh

--- jprofiler.sh
+++ jprofiler.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 
 VERSION=$(cat jprofiler-archives/version)
 
-cp jprofiler-archives/jprofiler_linux_*.tar.gz repository/jprofiler-$VERSION.tar.gz
+unzip -qq jprofiler-archives/jprofiler-archives/jprofiler_linux_*.tar.gz
+
+cp jprofiler*/bin/linux-x64/libjprofilerti.so repository/jprofiler-${VERSION}.so
 cp jprofiler-archives/version repository/version
